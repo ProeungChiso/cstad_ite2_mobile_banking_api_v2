fd7baa90bca4b5955711c9ec481f93477a57850f
ProeungChiso <proeung.chiso.2821@rupp.edu.kh>
2024-03-31 00:26:21 +0700

    starting

---
+++ src/main/java/co/istad/ite2_mbanking_api_v2/Ite2MbankingApiV2Application.java
@@ -0,0 +1,13 @@
+package co.istad.ite2_mbanking_api_v2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ite2MbankingApiV2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ite2MbankingApiV2Application.class, args);
+    }
+
+}
---
+++ src/test/java/co/istad/ite2_mbanking_api_v2/Ite2MbankingApiV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package co.istad.ite2_mbanking_api_v2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ite2MbankingApiV2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
